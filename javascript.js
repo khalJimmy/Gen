2869eddf5f852880c95aa33e9f6b974110902d2c
khalJimmy <63753264+khalJimmy@users.noreply.github.com>
2020-07-10 04:17:32 +0530

    Greys character

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var quotes = [ 'alleluia', 'buking','xenon', 'cardied','corurtney','khalid','chrissy']
+var quotes = [ 'Alex', 'Mer','Sloan', 'Arizona','Lexipedia','Kepner','Webber','Miranda','Shepherd', 'Teddy', 'Owens', 'Callie', 'Christina', 'Burke', 'Addison']
 
 function newquote(){
   var randomNum = Math.floor( Math.random()*(quotes.length));
