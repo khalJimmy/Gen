5ff37d5d8c2437d486c099f6f2f02cfe91582665
khalJimmy <63753264+khalJimmy@users.noreply.github.com>
2020-07-24 02:50:26 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-var quotes = [ 'Alex', 'Mer','Sloan', 'Arizona','Lexipedia','Kepner','Webber','Miranda','Shepherd', 'Teddy', 'Owens', 'Callie', 'Christina', 'Burke', 'Addison']
+var str="Hamilton Burr Eliza Angelica Peggy Lafayette KingGeorge Theodosia Reynold JohnAdams Jefferson Washington";
+var quotes = str.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
+
 
 function newquote(){
   var randomNum = Math.floor( Math.random()*(quotes.length));
@@ -6,6 +8,32 @@ function newquote(){
    quotes[randomNum];
  }
  
+
+ 
+const hex=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+const btn= document.getElementById("btn");
+const color=document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+				
+				let hexcolor="#";
+				for(i=0;i<6;i++){
+								
+								hexcolor+=hex[getRandom()];
+								
+								}
+				
+				document.body.style.backgroundColor=hexcolor;
+				
+				});
+				
+function getRandom(){
+				return Math.floor(Math.random() * hex.length);
+				
+				
+				}
+
  
 
  
